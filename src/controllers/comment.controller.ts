62204a1a2603ad3da190ce89959fe3d9b8cb36c4
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2023-04-25 22:28:21 -0500

    Modificación

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -1,5 +1,5 @@
-import Express, { Request, Response, NextFunction }  from "express";
-import  path from "path" ;
+import Express, { Request, Response, NextFunction } from "express";
+import path from "path";
 import fs from "fs";
 import sequelize from "../database/mysql.db.js";
 import commentSeccionModel from "../models/commentsection.model.js";
@@ -11,17 +11,17 @@ import commentimageModel from "../models/commentimage.model.js";
 const createComment = async (req: Request, res: Response, next: NextFunction) => {
     const fechaActual: Date = new Date();
     const dia: number = fechaActual.getDate();
-    const mes: number = fechaActual.getMonth() + 1; 
+    const mes: number = fechaActual.getMonth() + 1;
     const anio: number = fechaActual.getFullYear();
     const fechaFormateada: string = `${dia}/${mes}/${anio}`;
-    
+
     console.log(fechaFormateada);
 
     try {
         const { id_usuario, comentario, valoracion } = req.body;
         const { id_producto } = req.params;
         var commentSeccion = await commentSeccionModel.findOne({
-            where:{
+            where: {
                 Producto_id_producto: id_producto,
                 Usuario_id_usuario: id_usuario,
             }
@@ -38,7 +38,7 @@ const createComment = async (req: Request, res: Response, next: NextFunction) =>
             fecha_comentario: fechaFormateada,
             Seccion_Comentario_id_comentario: commentSeccion.id_seccion_comentario!,
         });
-        (req.files as unknown as globalThis.Express.Multer.File[]).forEach(async(file) =>{
+        (req.files as unknown as globalThis.Express.Multer.File[]).forEach(async (file) => {
             var id_image = path.parse(file.filename).name;
 
             await commentimageModel.create({
@@ -51,46 +51,124 @@ const createComment = async (req: Request, res: Response, next: NextFunction) =>
 
     } catch (error) {
         console.log(error),
-        res.status(500).json({ status: false, message: 'Server internal error' });
+            res.status(500).json({ status: false, message: 'Server internal error' });
     }
 }
 
-const getCommentImage = async (req:Request, res:Response, next: NextFunction)=>{
-    try{
-        const {id_comment} = req.params;
+const getCommentImage = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id_comment } = req.params;
         var route = path.join(__dirname, `../../../uploads/comment_images/${id_comment}.jpg`);
-        fs.open(route, 'r', (err, df)=>{
-            if(err) 
+        fs.open(route, 'r', (err, df) => {
+            if (err)
 
-            res.status(404).json({error: 0, message: "Image not found"});
-            else{
+                res.status(404).json({ error: 0, message: "Image not found" });
+            else {
                 res.sendFile(route);
             }
         });
-    }catch(error){
-        res.status(500).json({error: 1, status: false});
+    } catch (error) {
+        res.status(500).json({ error: 1, status: false });
     }
-} 
+}
 
 
 const getAllComment = async (req: Request, res: Response, next: NextFunction) => {
-    
+
     const { id_producto } = req.params;
-    try{
-        const seccioncomments = await sequelize.query('CALL get_comments(:_id_seccion_comentario)', {replacements: {_id_seccion_comentario : id_producto}});
-        if(seccioncomments){
-            res.status(200).json({error: 0, status: true, comments: seccioncomments });
-        }else{
-            res.status(400).json({error: 0, status: true, message: 'No hay seccion de comentarios' });
+    try {
+        const seccioncomments = await sequelize.query('CALL get_comments(:_id_seccion_comentario)', { replacements: { _id_seccion_comentario: id_producto } });
+        if (seccioncomments) {
+            res.status(200).json({ error: 0, status: true, comments: seccioncomments });
+        } else {
+            res.status(400).json({ error: 0, status: true, message: 'No hay seccion de comentarios' });
         }
-    }catch(error){
+    } catch (error) {
         console.log(error)
-        res.status(500).json({error: 0, status: false, message: 'Server internal error.'});
+        res.status(500).json({ error: 0, status: false, message: 'Server internal error.' });
     }
 }
+
+
+
+const editComment = async (req: Request, res: Response, next: NextFunction) => {
+    const fechaActual: Date = new Date();
+    const dia: number = fechaActual.getDate();
+    const mes: number = fechaActual.getMonth() + 1;
+    const anio: number = fechaActual.getFullYear();
+    const fechaFormateada: string = `${dia}/${mes}/${anio}`;
+
+    try {
+        const { id_usuario, comentario, valoracion } = req.body;
+        const { id_producto, id_comentario } = req.params;
+
+        const commentSeccion = await commentSeccionModel.findOne({
+            where: {
+                Producto_id_producto: id_producto,
+                Usuario_id_usuario: id_usuario,
+            }
+        });
+
+        if (!commentSeccion) {
+            return res.status(404).json({ status: false, message: 'Comment section not found' });
+        }
+
+        const commentdetails = await commentdetailsModel.findOne({
+            where: {
+                id_detalles_comentario: id_comentario,
+                Seccion_Comentario_id_comentario: commentSeccion.id_seccion_comentario!
+            }
+        });
+
+        if (!commentdetails) {
+            return res.status(404).json({ status: false, message: 'Comment not found' });
+        }
+
+        commentdetails.comentario = comentario;
+        commentdetails.fecha_comentario = fechaFormateada;
+        commentSeccion.Valoracion = valoracion;
+
+        await commentdetails.save();
+        await commentSeccion.save();
+
+        res.status(200).json({ status: true });
+    } catch (error) {
+        console.log(error),
+            res.status(500).json({ status: false, message: 'Server internal error' });
+    }
+}
+
+
+const deleteComment = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id_comment } = req.params;
+
+        const comment = await commentdetailsModel.findByPk(id_comment);
+
+        if (!comment) {
+            return res.status(404).json({ error: 0, message: "Comment not found" });
+        }
+
+        await commentimageModel.destroy({
+            where: {
+                Detalles_Comentario_idDetalles_Comentario: id_comment,
+            },
+        });
+
+        await comment.destroy();
+
+        res.status(200).json({ error: 0, status: true });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 1, status: false, message: "Server internal error" });
+    }
+};
+
 
 export default {
     createComment,
     getCommentImage,
     getAllComment,
+    editComment,
+    deleteComment
 }
